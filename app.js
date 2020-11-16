188c318ca9873bb5a7bdbbaf02837d3f974bf449
Tim Figueroa <timothy@psalmsimageplus.com>
2020-11-17 05:14:17 +0800

    sample change to app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Youtube!"));
+app.get("/", (req, res) => res.send("Hydroponics test website"));
 
 app.get("/health", (req, res) => {
   res.status(200);
